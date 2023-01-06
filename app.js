e2b3f273b5fb6bf35aee9817b60f3ed16424ebfa
Haden Liu <haden.liui30@gmail.com>
2023-01-06 18:15:45 +1100

    haden

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-
-
 require('dotenv').config()
 const express = require("express")
 const bodyParser = require("body-parser")
@@ -8,11 +6,7 @@ const bcrypt = require("bcrypt")
 const expressSession = require("express-session")
 const pgSession = require("connect-pg-simple")(expressSession)
 const cloudinary = require("cloudinary").v2
-
-const cors = require("cors")
-
 const cookieParser = require("cookie-parser")
-
 
 const app = express()
 
@@ -40,11 +34,6 @@ app.use(expressSession({
     secret: process.env.SECRET,
 }))
 
-app.use(cors({
-    credentials: true,
-}));
-
-
 // ------------------------ //
 // -------- Routes -------- //
 // ------------------------ //
@@ -64,7 +53,6 @@ app.get("/api/profile/:user_id", (req, res) => {
     const sql = "SELECT * FROM users WHERE user_id = $1"
     const params = [req.params.user_id]
     db.query(sql, params).then((dbResult) => {
-        // const userID = req.params.user_id
         res.json(dbResult.rows[0]) 
     })
 })
@@ -87,23 +75,19 @@ app.post("/api/login-session", (req, res) => {
             if (isValidPassword(password, user.password)) {
                 req.session.email = email
                 req.session.user_id = user.user_id
-
-                console.log(req.session.user_id)
-
                 req.session.save()
 
                 // console.log(req.session)
                 // console.log('the req session is' + req.session, req.session.user_id, user.user_id)
 
                 res.json({message: "Login Successful"})
-
                 
             } else {
                 res.status(401).json({message: "Invalid password"})
             }
         }
     })
-});
+})
 
 // get calories routes
 
@@ -115,6 +99,26 @@ app.get("/profile/calaries/:id", (req, res) => {
         res.json(rows)
     })
 })
+
+// App route to get weight information for user
+app.get("/api/weight/:id", (req, res) =>{
+    const sql = "SELECT * FROM weight_tracker WHERE user_id = $1"
+    const params = [req.params.id]
+    db.query(sql, params).then((response) => {
+        // response.rows is an array of objects
+        res.json(response.rows) 
+    })
+})
+
+// App route to get activity information for user
+app.get("/api/activity/:id", (req, res) =>{
+    const sql = "SELECT * FROM activity_tracker WHERE user_id = $1"
+    const params = [req.params.id]
+    db.query(sql, params).then((response) => {
+        // response.rows is an array of objects
+        res.json(response.rows) 
+    })
+});
 
 app.get("/api/session", (req, res) => {
     res.json(req.session)
@@ -206,3 +210,4 @@ app.put("/api/accounts/:id", (req, res) => {
 app.listen(PORT, () => {
     console.log(`Connected on http://localhost:${PORT}`)
 })
+
--- static/js/initialize.js
+++ static/js/initialize.js
@@ -3,17 +3,16 @@ import { renderFooter } from './components/footer.js'
 import { renderHome } from './components/homeDashboard.js'
 import { renderProfileDescription } from './components/profileDescription.js'
 
-import { renderCalaries } from './components/calariesList.js'
-
-
 import { renderWeightPage } from './components/weight.js'
 import { renderActivityPage } from './components/activity.js'
 
 renderWeightPage()
 renderActivityPage()
-
 renderHome()
 renderHeader()
 renderProfileDescription()
 renderFooter()
+
+
 renderCalaries()
+import { renderCalaries } from './components/calariesList.js'
