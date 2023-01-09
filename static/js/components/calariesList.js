86bb2865ec8a5cfde216eaa9610af4f3b69a4da0
Haden Liu <haden.liui30@gmail.com>
2023-01-09 20:05:56 +1100

    add session for cal

--- static/js/components/calariesList.js
+++ static/js/components/calariesList.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 
 
 export function renderCalaries() {
@@ -10,48 +11,49 @@ export function renderCalaries() {
     // const cal_section = document.createElement('div')
     // cal_section.id = 'cal_section'
 
-
-
     axios
-        .get('http://localhost:3000/profile/calaries/1')
-        .then((response) => {
-            const lists = response.data
-            console.log(lists)
 
-            const cal_section = document.createElement('div')
-            cal_section.id = 'cal_section'
+    .get("/api/session")
+    .then((response) => {
+        console.log(response.data)
+        const user_id = response.data.user_id
 
-            lists.forEach(list=> {
-            console.log(list)
-            const user_id = list.user_id
-            const user_name = list.f_name
-            console.log(user_id)
-            const date = new Date(list.date)
-            let year = date.getFullYear(date)
-            let month = date.getMonth(date)
-            let day = date.getDay(date)
-            let d = date.getDate()
-            const calories = list.calories
+        axios
+            .get(`http://localhost:3000/profile/calaries/${user_id}`)
+            .then((response) => {
+                const lists = response.data
+                console.log(lists)
 
-            cal_section.innerHTML = `
-            <div class="main-card-container">
-            <div class="page-title">
-            <h1>Calories</h1>
-            </div>
-            <div id="cal_rendering">
-                <h2>User Name: ${user_name}</h2>
-                <h2>Acitivty Date: ${day} - ${month} - ${year}</h2>
-                <h2>Calaries Consumed: ${calories} Calaries</h2>
-            </div>
-            </div>
-            </div>
-            `})
-            mainContainer.appendChild(cal_section)
+                const cal_section = document.createElement('div')
+                cal_section.id = 'cal_section'
 
-        
-        
-        
-        
-        
-        })   
-        }
+                lists.forEach(list=> {
+                console.log(list)
+                const user_id = list.user_id
+                const user_name = list.f_name
+                console.log(user_id)
+                const date = new Date(list.date)
+                let year = date.getFullYear(date)
+                let month = date.getMonth(date)
+                let day = date.getDay(date)
+                let d = date.getDate()
+                const calories = list.calories
+
+                cal_section.innerHTML = `
+                <div class="main-card-container">
+                <div class="page-title">
+                <h1>Calories</h1>
+                </div>
+                <div id="cal_rendering">
+                    <h2>User Name: ${user_name}</h2>
+                    <h2>Acitivty Date: ${day} - ${month} - ${year}</h2>
+                    <h2>Calaries Consumed: ${calories} Calaries</h2>
+                </div>
+                </div>
+                </div>
+                `})
+                mainContainer.appendChild(cal_section)   
+            
+            })   
+            })
+            }
