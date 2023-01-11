cbc5e5f4435401276bb01d4a83e05595e37183c3
Stella Ma <stella.ma96@gmail.com>
2023-01-11 18:36:18 +1100

    added sessions for weight and activity pages

--- static/js/components/activity.js
+++ static/js/components/activity.js
@@ -10,38 +10,43 @@ export function renderActivityPage() {
 
     // Appending this to the main container section of the html page.
     mainContainer.appendChild(activityContainer)
-
     axios
-        // Getting the JSON information from the activity table for user x (in this case user 1)
-        .get("/api/activity/1")
-        // Using this response to display information on html page.
+        .get("api/session")
         .then((response) => {
-            console.log(response.data)
-            // dbResponse is an array of objects. Each object is an entry of the user's date, activity, user id and activity tracker id.
-            const dbResponse = response.data
-            console.log("database response is: ", dbResponse)
+            const user_id = response.data.user_id
+            axios
+                // Getting the JSON information from the activity table for user x (in this case user 1)
 
-            // Loop through each activity entry of the user to get the date and activity
-            for (let i = 0; i < dbResponse.length; i++) {   
-                // Storing the activity and date from the db response into variables
-                const activity = dbResponse[i].activities
-                const date = dbResponse[i].date
+                .get(`/api/activity/${user_id}`)
+                // Using this response to display information on html page.
+                .then((response) => {
+                    console.log(response.data)
+                    // dbResponse is an array of objects. Each object is an entry of the user's date, activity, user id and activity tracker id.
+                    const dbResponse = response.data
+                    console.log("database response is: ", dbResponse)
 
-                console.log("date array is: ", date)
-                console.log("activity array is: ", activity)
+                    // Loop through each activity entry of the user to get the date and activity
+                    for (let i = 0; i < dbResponse.length; i++) {
+                        // Storing the activity and date from the db response into variables
+                        const activity = dbResponse[i].activities
+                        const date = dbResponse[i].date
 
-                // Creating a new div for each entry
-                const dailyActivity = document.createElement("div")
+                        console.log("date array is: ", date)
+                        console.log("activity array is: ", activity)
 
-                // Inputting the newly created div with the Activity and date
-                dailyActivity.innerHTML = `
-                    <p>Date: ${date}: <br>
-                    Activity: ${activity}</p>
-                `
+                        // Creating a new div for each entry
+                        const dailyActivity = document.createElement("div")
 
-                // Appending each entry to the overall activity container.
-                activityContainer.appendChild(dailyActivity)
-            }
+                        // Inputting the newly created div with the Activity and date
+                        dailyActivity.innerHTML = `
+                        <p>Date: ${date}: <br>
+                        Activity: ${activity}</p>`
+
+                        // Appending each entry to the overall activity container.
+                        activityContainer.appendChild(dailyActivity)
+                    }
+                })
         })
+
 
 }
